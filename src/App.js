f7d77a7b2d046e0a0f50842c3851f43823f56dcb
mint5150 <musia.home@gmail.com>
2024-01-28 20:44:51 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Tictactoe from './Tictactoe';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Tictactoe />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          メインサイトは<a href="https://shun-wordpress.com/reactjs/">こちら</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+//react ライブラリから Reactを読み込み
 import React from 'react';
+//Web ブラウザとやり取りするための React ライブラリ (React DOM)を読み込み
 import ReactDOM from 'react-dom/client';
+//CSSを読み込み
 import './index.css';
+//App.jsを読み込み
 import App from './App';
+//reportWebVitalsを読み込み
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,8 +15,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
